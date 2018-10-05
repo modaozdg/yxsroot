2fe4de4a3516b8c780ebd3e8869b6f7a5f6eb627
modaozdg <296980893@qq.com>
2018-10-05 18:48:59 +0800

    新admin模块和model模块

--- yxs-admin/src/main/java/com/yxs/admin/controller/IndexController.java
+++ yxs-admin/src/main/java/com/yxs/admin/controller/IndexController.java
@@ -1,7 +1,18 @@
 package com.yxs.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+
+    @RequestMapping(value = {"/","/index"})
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping(value = "/home/console")
+    public String console(){
+        return "home/console";
+    }
 }
